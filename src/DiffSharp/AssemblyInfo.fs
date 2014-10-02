d62b9654ed5244a5eecd7eed7786395b7c0a0391
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2014-10-02 15:56:36 +0100

    Assembly info

---
+++ src/DiffSharp/AssemblyInfo.fs
@@ -0,0 +1,42 @@
+﻿#light
+ 
+namespace DiffSharp
+
+open System.Reflection;
+open System.Runtime.CompilerServices;
+open System.Runtime.InteropServices;
+ 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("DiffSharp")>]
+[<assembly: AssemblyDescription("DiffSharp: Automatic Differentiation Library")>]
+[<assembly: AssemblyConfiguration("Debug")>]
+[<assembly: AssemblyCompany("National University of Ireland Maynooth (Atilim Gunes Baydin, Barak A. Pearlmutter)")>]
+[<assembly: AssemblyProduct("DiffSharp")>]
+[<assembly: AssemblyCopyright("Copyright (c) 2014, National University of Ireland Maynooth (Atilim Gunes Baydin, Barak A. Pearlmutter)")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+ 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("D042C8CF-9C0C-444C-A417-F407C3DD687D")>]
+ 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the ‘*’ as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("0.5.0.0")>]
+[<assembly: AssemblyInformationalVersion("0.5.0.0")>]
+[<assembly: AssemblyFileVersion("0.5.0.*")>]
+()
