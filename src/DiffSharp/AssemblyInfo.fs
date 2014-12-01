92fa45d75c78ca7aeecb41e3aaa344e1bee555e0
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2014-12-01 02:13:23 +0000

    Moving to new version

--- src/DiffSharp/AssemblyInfo.fs
+++ src/DiffSharp/AssemblyInfo.fs
@@ -36,9 +36,9 @@ open System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the ‘*’ as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("0.5.4")>]
-[<assembly: AssemblyInformationalVersion("0.5.4")>]
-[<assembly: AssemblyFileVersion("0.5.4.*")>]
+[<assembly: AssemblyVersion("0.5.5")>]
+[<assembly: AssemblyInformationalVersion("0.5.5")>]
+[<assembly: AssemblyFileVersion("0.5.5.*")>]
 
 [<assembly: AutoOpen("DiffSharp.Util.LinearAlgebra")>]
 ()
