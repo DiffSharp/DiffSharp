6a0681dcab05a3e06536c7aa5be2bc33cbc873d5
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-03-28 19:18:47 +0000

    Nesting comments

--- src/DiffSharp/AD.Nested.Forward.fs
+++ src/DiffSharp/AD.Nested.Forward.fs
@@ -36,11 +36,13 @@
 
 #light
 
+/// Nested forward mode AD module
 module DiffSharp.AD.Nested.Forward
 
 open DiffSharp.Util.LinearAlgebra
 open DiffSharp.Util.General
 
+/// Dual numeric type with nesting capability, using tags to avoid perturbation confusion
 [<CustomEquality; CustomComparison>]
 type D =
     | Df of float
@@ -68,6 +70,7 @@ type D =
         match d with
         | Df(a) -> hash [| a |]
         | D(ap, at, ai) -> hash [|ap; at; ai|]
+        // D - D binary operations
     static member (+) (a:D, b:D) =
         match a, b with
         | Df(a),         Df(b)         -> Df(a + b)
@@ -116,6 +119,7 @@ type D =
         | D(ap, at, ai), D(bp, bt, bi) when ai < bi -> D(atan2 a bp, -(a * bt) / (a * a + bp * bp), bi)
         | D(ap, at, ai), D(bp, bt, bi) when ai = bi -> D(atan2 ap bp, (at * bp - ap * bt) / (ap * ap + bp * bp), ai)
         | D(ap, at, ai), D(bp, bt, bi) when ai > bi -> D(atan2 ap b, (at * b) / (ap * ap + b * b), ai)
+    // D - float binary operations
     static member (+) (a:D, b:float) =
         match a with
         | Df(a) -> Df(a + b)
@@ -140,6 +144,7 @@ type D =
         match a with
         | Df(a) -> Df(atan2 a b)
         | D(ap, at, ai) -> D(D.Atan2(ap, b), (b * at) / (b * b + ap * ap), ai)
+    // float - D binary operations
     static member (+) (a:float, b:D) =
         match b with
         | Df(b) -> Df(a + b)
@@ -164,6 +169,7 @@ type D =
         match b with
         | Df(b) -> Df(atan2 a b)
         | D(bp, bt, bi) -> D(D.Atan2(a, bp), -(a * bt) / (a * a + bp * bp), bi)
+    // D unary operations
     static member Log (a:D) =
         if (float a) <= 0. then invalidArgLog()
         match a with
@@ -257,19 +263,23 @@ type Tagger =
     new(t) = {LastTag = t}
     member t.Next() = t.LastTag <- t.LastTag + 1UL; t.LastTag
 
+/// Global tagger for D operations
 type GlobalTagger() =
     static let T = new Tagger(0UL)
     static member Next = T.Next()
     static member Reset = T.LastTag <- 0UL
 
 
+/// D operations module (automatically opened)
 [<AutoOpen>]
 module DOps =
+    /// Make D, with primal value `p`
     let inline dual p =
         match box p with
         | :? float as p -> Df(p)
         | :? int as p -> Df(float p)
         | :? D as p -> p
+    /// Make D, with tag `i`, primal value `p`, and tangent value `t`
     let inline dualIPT i p t =
         match box p with
         | :? float as p ->
@@ -287,7 +297,9 @@ module DOps =
                 | D(pp,pt,pi), D(tp,tt,ti) when pi < ti -> D(D(pp,Df 0.,ti), D(tp,tt,ti), i)
                 | D(pp,pt,pi), D(tp,tt,ti) when pi = ti -> D(D(pp,pt,pi), D(tp,tt,pi), i)
                 | D(pp,pt,pi), D(tp,tt,ti) when pi > ti -> D(D(pp,pt,pi), D(tp,Df 0.,pi), i)
+    /// Make D, with primal value `p` and tangent value `t`. A new tag will be attached using the global tagger.
     let inline dualPT p t = dualIPT GlobalTagger.Next p t
+    /// Make D, with primal tag `i` and primal value `p`, and tangent 1.
     let inline dualIP1 i p =
         match box p with
         | :? float as p -> D(Df p, Df 1., i)
@@ -295,15 +307,19 @@ module DOps =
             match p with
             | Df(_) -> D(p, Df 1., i)
             | D(_, _, i) -> D(p, D(Df 1., Df 0., i), i)
+    /// Make D, with primal value `p` and tangent 1. A new tag will be attached using the global tagger.
     let inline dualP1 p = dualIP1 GlobalTagger.Next p
+    /// Get the primal value of a D
     let inline primal (d:D) =
         match d with
         | Df(_) -> d
         | D(p,_,_) -> p
+    /// Get the tangent value of a D
     let inline tangent (d:D) =
         match d with
         | Df(_) -> Df(0.)
         | D(_,t,_) -> t
+    /// Get the primal and the first gradient component of a D, as a tuple
     let inline tuple (d:D) =
         match d with
         | Df(_) -> (d, Df 0.)
@@ -312,24 +328,29 @@ module DOps =
 
 [<AutoOpen>]
 module ForwardOps =
-    
+    /// Original value and first derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff' f x =
         x |> dualP1 |> f |> tuple
 
+    /// First derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff f x =
         x |> dualP1 |> f |> tangent
 
+    /// Second derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff2 f x =
         diff (diff f) x
 
+    /// Original value, first derivative, and second derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff2'' f x =
         let v, d = diff' f x
         let d2 = diff2 f x
         (v, d, d2)
 
+    /// Original value and second derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff2' f x =
         diff2'' f x |> fsttrd
 
+    /// `n`-th derivative of a scalar-to-scalar function `f`, at point `x`
     let diffn n f x =
         if n < 0 then invalidArg "" "Order of differentiation cannot be negative."
         elif n = 0 then x |> dual |> f
@@ -339,24 +360,30 @@ module ForwardOps =
                 | 1 -> f
                 | _ -> d (n - 1) (diff f)
             x |> dualP1 |> (d n f) |> tangent
-
+    
+    /// Original value and `n`-th derivative of a scalar-to-scalar function `f`, at point `x`
     let diffn' n f x =
         (diffn 0 f x, diffn n f x)
 
+    /// Original value and gradient-vector product (directional derivative) of a vector-to-scalar function `f`, at point `x`, along vector `v`
     let inline gradv' f x v =
         let i = GlobalTagger.Next
         Array.map2 (dualIPT i) x v |> f |> tuple
 
+    /// Gradient-vector product (directional derivative) of a vector-to-scalar function `f`, at point `x`, along vector `v`
     let inline gradv f x v =
         gradv' f x v |> snd
 
+    /// Original value and gradient of a vector-to-scalar function `f`, at point `x`
     let inline grad' f (x:_[]) =
         let a = Array.init x.Length (fun i -> gradv' f x (standardBasis x.Length i))
         (fst a.[0], Array.map snd a)
 
+    /// Gradient of a vector-to-scalar function `f`, at point `x`
     let inline grad f x =
         grad' f x |> snd
 
+    /// Original value and Laplacian of a vector-to-scalar function `f`, at point `x`
     let inline laplacian' f (x:_[]) =
         let i = GlobalTagger.Next
         let a = Array.init x.Length (fun j ->
@@ -366,94 +393,140 @@ module ForwardOps =
                                         <| xd.[j])
         (x |> Array.map dual |> f, Array.sumBy tangent a)
 
+    /// Laplacian of a vector-to-scalar function `f`, at point `x`
     let inline laplacian f x =
         laplacian' f x |> snd
 
+    /// Original value and Jacobian-vector product of a vector-to-vector function `f`, at point `x`, along vector `v`
     let inline jacobianv' f x v =
         let i = GlobalTagger.Next
         Array.map2 (dualIPT i) x v |> f |> Array.map tuple |> Array.unzip
 
+    /// Jacobian-vector product of a vector-to-vector function `f`, at point `x`, along vector `v`
     let inline jacobianv f x v =
         jacobianv' f x v |> snd
 
+    /// Original value and transposed Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobianT' f (x:_[]) =
         let a = Array.init x.Length (fun i -> jacobianv' f x (standardBasis x.Length i))
         (fst a.[0], array2D (Array.map snd a))
 
+    /// Transposed Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobianT f x =
         jacobianT' f x |> snd
 
+    /// Original value and Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobian' f x =
         jacobianT' f x |> fun (r, j) -> (r, transpose j)
 
+    /// Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobian f x =
         jacobian' f x |> snd
 
+    /// Gradient and Hessian of a vector-to-scalar function `f`, at point `x`
     let inline gradhessian f x =
         jacobian' (grad f) x
 
+    /// Original value, gradient, and Hessian of a vector-to-scalar function `f`, at point `x`
     let inline gradhessian' f x =
         let g, h = gradhessian f x
         (x |> Array.map dual |> f, g, h)
 
+    /// Hessian of a vector-to-scalar function `f`, at point `x`
     let inline hessian f x =
         jacobian (grad f) x
 
+    /// Original value and Hessian of a vector-to-scalar function `f`, at point `x`
     let inline hessian' f x =
         (x |> Array.map dual |> f, hessian f x)
 
+    /// Original value and curl of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curl' f x =
         let v, j = jacobianT' f x
         if (Array2D.length1 j, Array2D.length2 j) <> (3, 3) then invalidArgCurl()
         v, [|j.[1, 2] - j.[2, 1]; j.[2, 0] - j.[0, 2]; j.[0, 1] - j.[1, 0]|]
 
+    /// Curl of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curl f x =
         curl' f x |> snd
 
+    /// Original value and divergence of a vector-to-vector function `f`, at point `x`. Defined only for functions with a square Jacobian matrix.
     let inline div' f x =
         let v, j = jacobianT' f x
         if Array2D.length1 j <> Array2D.length2 j then invalidArgDiv()
         v, trace j
 
+    /// Divergence of a vector-to-vector function `f`, at point `x`. Defined only for functions with a square Jacobian matrix.
     let inline div f x =
         div' f x |> snd
 
+    /// Original value, curl, and divergence of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curldiv' f x =
         let v, j = jacobianT' f x
         if (Array2D.length1 j, Array2D.length2 j) <> (3, 3) then invalidArgCurlDiv()
         v, [|j.[1, 2] - j.[2, 1]; j.[2, 0] - j.[0, 2]; j.[0, 1] - j.[1, 0]|], trace j
 
+    /// Curl and divergence of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curldiv f x =
         curldiv' f x |> sndtrd
 
 
 module Vector =
+    /// Original value and first derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff' (f:D->D) x = ForwardOps.diff' f x
+    /// First derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff (f:D->D) x = ForwardOps.diff f x
+    /// Original value and second derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff2' (f:D->D) x = ForwardOps.diff2' f x
+    /// Second derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff2 (f:D->D) x = ForwardOps.diff2 f x
+    /// Original value, first derivative, and second derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diff2'' (f:D->D) x = ForwardOps.diff2'' f x
-    let inline diffn' (n:int) (f:D->D) x = ForwardOps.diffn' n f x 
+    /// Original value and the `n`-th derivative of a scalar-to-scalar function `f`, at point `x`
+    let inline diffn' (n:int) (f:D->D) x = ForwardOps.diffn' n f x
+    /// `n`-th derivative of a scalar-to-scalar function `f`, at point `x`
     let inline diffn (n:int) (f:D->D) x = ForwardOps.diffn n f x
+    /// Original value and directional derivative of a vector-to-scalar function `f`, at point `x`, along vector `v`
     let inline gradv' (f:Vector<D>->D) x v = ForwardOps.gradv' (vector >> f) (Vector.toArray x) (Vector.toArray v)
+    /// Directional derivative of a vector-to-scalar function `f`, at point `x`, along vector `v`
     let inline gradv (f:Vector<D>->D) x v = ForwardOps.gradv (vector >> f) (Vector.toArray x) (Vector.toArray v)
+    /// Original value and gradient of a vector-to-scalar function `f`, at point `x`
     let inline grad' (f:Vector<D>->D) x = ForwardOps.grad' (vector >> f) (Vector.toArray x) |> fun (a, b) -> (a, vector b)
+    /// Gradient of a vector-to-scalar function `f`, at point `x`
     let inline grad (f:Vector<D>->D) x = ForwardOps.grad (vector >> f) (Vector.toArray x) |> vector
+    /// Original value and Laplacian of a vector-to-scalar function `f`, at point `x`
     let inline laplacian' (f:Vector<D>->D) x = ForwardOps.laplacian' (vector >> f) (Vector.toArray x)
+    /// Laplacian of a vector-to-scalar function `f`, at point x
     let inline laplacian (f:Vector<D>->D) x = ForwardOps.laplacian (vector >> f) (Vector.toArray x)
+    /// Original value and transposed Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobianT' (f:Vector<D>->Vector<_>) x = ForwardOps.jacobianT' (vector >> f >> Vector.toArray) (Vector.toArray x) |> fun (a, b) -> (vector a, Matrix.ofArray2D b)
+    /// Transposed Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobianT (f:Vector<D>->Vector<_>) x = ForwardOps.jacobianT (vector >> f >> Vector.toArray) (Vector.toArray x) |> Matrix.ofArray2D
+    /// Original value and Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobian' (f:Vector<D>->Vector<_>) x = ForwardOps.jacobian' (vector >> f >> Vector.toArray) (Vector.toArray x) |> fun (a, b) -> (vector a, Matrix.ofArray2D b)
+    /// Jacobian of a vector-to-vector function `f`, at point `x`
     let inline jacobian (f:Vector<D>->Vector<_>) x = ForwardOps.jacobian (vector >> f >> Vector.toArray) (Vector.toArray x) |> Matrix.ofArray2D
+    /// Original value and Jacobian-vector product of a vector-to-vector function `f`, at point `x`, along vector `v`
     let inline jacobianv' (f:Vector<D>->Vector<D>) x v = ForwardOps.jacobianv' (vector >> f >> Vector.toArray) (Vector.toArray x) (Vector.toArray v) |> fun (a, b) -> (vector a, vector b)
+    /// Jacobian-vector product of a vector-to-vector function `f`, at point `x`, along vector `v`
     let inline jacobianv (f:Vector<D>->Vector<D>) x v = ForwardOps.jacobianv (vector >> f >> Vector.toArray) (Vector.toArray x) (Vector.toArray v) |> vector
+    /// Hessian of a vector-to-scalar function `f`, at point `x`
     let inline hessian (f:Vector<D>->D) x = ForwardOps.hessian (vector >> f) (Vector.toArray x) |> Matrix.ofArray2D
+    /// Original value and Hessian of a vector-to-scalar function `f`, at point `x`
     let inline hessian' (f:Vector<D>->D) x = ForwardOps.hessian' (vector >> f) (Vector.toArray x) |> fun (a, b) -> (a, Matrix.ofArray2D b)
+    /// Original value, gradient, and Hessian of a vector-to-scalar function `f`, at point `x`
     let inline gradhessian' (f:Vector<D>->D) x = ForwardOps.gradhessian' (vector >> f) (Vector.toArray x) |> fun (a, b, c) -> (a, vector b, Matrix.ofArray2D c)
+    /// Gradient and Hessian of a vector-to-scalar function `f`, at point `x`
     let inline gradhessian (f:Vector<D>->D) x = ForwardOps.gradhessian (vector >> f) (Vector.toArray x) |> fun (a, b) -> (vector a, Matrix.ofArray2D b)
+    /// Original value and curl of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curl' (f:Vector<D>->Vector<D>) x = ForwardOps.curl' (vector >> f >> Vector.toArray) (Vector.toArray x) |> fun (a, b) -> (vector a, vector b)
+    /// Curl of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curl (f:Vector<D>->Vector<D>) x = ForwardOps.curl (vector >> f >> Vector.toArray) (Vector.toArray x) |> vector
+    /// Original value and divergence of a vector-to-vector function `f`, at point `x`. Defined only for functions with a square Jacobian matrix.
     let inline div' (f:Vector<D>->Vector<D>) x = ForwardOps.div' (vector >> f >> Vector.toArray) (Vector.toArray x) |> fun (a, b) -> (vector a, b)
+    /// Divergence of a vector-to-vector function `f`, at point `x`. Defined only for functions with a square Jacobian matrix.
     let inline div (f:Vector<D>->Vector<D>) x = ForwardOps.div (vector >> f >> Vector.toArray) (Vector.toArray x)
+    /// Original value, curl, and divergence of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curldiv' (f:Vector<D>->Vector<D>) x = ForwardOps.curldiv' (vector >> f >> Vector.toArray) (Vector.toArray x) |> fun (a, b, c) -> (vector a, vector b, c)
+    /// Curl and divergence of a vector-to-vector function `f`, at point `x`. Supported only for functions with a three-by-three Jacobian matrix.
     let inline curldiv (f:Vector<D>->Vector<D>) x = ForwardOps.curldiv (vector >> f >> Vector.toArray) (Vector.toArray x) |> fun (a, b) -> (vector a, b)
