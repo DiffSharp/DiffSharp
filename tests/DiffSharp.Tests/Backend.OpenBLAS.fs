db61dfba1f842a5c9cdd4a4526984f8dc7ad42f9
Atılım Güneş Baydin <atilimgunes.baydin@gmail.com>
2015-12-14 02:42:27 +0000

    More tests

--- tests/DiffSharp.Tests/Backend.OpenBLAS.fs
+++ tests/DiffSharp.Tests/Backend.OpenBLAS.fs
@@ -1,30 +1,32 @@
 ﻿module DiffSharp.Tests.Backend.OpenBLAS
 
 open FsCheck.NUnit
-open DiffSharp.Config
-open DiffSharp.Tests.Util
+open DiffSharp.Backend
+open DiffSharp.Tests
 
+let Float32Backend = DiffSharp.Backend.OpenBLAS.Float32Backend() :> Backend<float32>
+let Float64Backend = DiffSharp.Backend.OpenBLAS.Float64Backend() :> Backend<float>
 
 // float32
 [<Property>]
 let ``OpenBLAS.32.Mul_Dot_V_V``(v:float32[]) = 
     let r = v |> Array.map (fun x -> x * x) |> Array.sum
-    (GlobalConfig.Float32Backend.Mul_Dot_V_V(v, v)) =~ r
+    Util.(=~)(Float32Backend.Mul_Dot_V_V(v, v), r)
 
 [<Property>]
 let ``OpenBLAS.32.L1Norm_V``(v:float32[]) = 
     let r = v |> Array.map abs |> Array.sum
-    (GlobalConfig.Float32Backend.L1Norm_V(v)) =~ r
+    Util.(=~)(Float32Backend.L1Norm_V(v), r)
 
 [<Property>]
 let ``OpenBLAS.32.L2Norm_V``(v:float32[]) = 
     let r = v |> Array.map (fun x -> x * x) |> Array.sum |> sqrt
-    (GlobalConfig.Float32Backend.L2Norm_V(v)) =~ r
+    Util.(=~)(Float32Backend.L2Norm_V(v), r)
 
 [<Property>]
 let ``OpenBLAS.32.Sum_V``(v:float32[]) = 
     let r = v |> Array.sum
-    (GlobalConfig.Float32Backend.Sum_V(v)) =~ r
+    Util.(=~)(Float32Backend.Sum_V(v), r)
 
 [<Property>]
 let ``OpenBLAS.32.Sum_M``(m:float32[,]) = 
@@ -32,28 +34,53 @@ let ``OpenBLAS.32.Sum_M``(m:float32[,]) =
     for i = 0 to (Array2D.length1 m) - 1 do
         for j = 0 to (Array2D.length2 m) - 1 do
             r <- r + m.[i, j]
-    (GlobalConfig.Float32Backend.Sum_M(m)) =~ r
+    Util.(=~)(Float32Backend.Sum_M(m), r)
 
-// float
+[<Property>]
+let ``OpenBLAS.32.Add_V_V``(v:float32[]) = 
+    let r = Array.map2 (+) v v
+    Util.(=~)(Float32Backend.Add_V_V(v, v), r)
+
+[<Property>]
+let ``OpenBLAS.32.Add_S_V``(s:float32, v:float32[]) = 
+    let r = v |> Array.map ((+) s)
+    Util.(=~)(Float32Backend.Add_S_V(s, v), r)
+
+[<Property>]
+let ``OpenBLAS.32.Sub_V_V``(v:float32[]) = 
+    let r = v |> Array.map (fun x -> x - x)
+    Util.(=~)(Float32Backend.Sub_V_V(v, v), r)
+
+[<Property>]
+let ``OpenBLAS.32.Sub_S_V``(s:float32, v:float32[]) = 
+    let r = v |> Array.map (fun x -> s - x)
+    Util.(=~)(Float32Backend.Sub_S_V(s, v), r)
+
+[<Property>]
+let ``OpenBLAS.32.Sub_V_S``(v:float32[], s:float32) = 
+    let r = v |> Array.map (fun x -> x - s)
+    Util.(=~)(Float32Backend.Sub_V_S(v, s), r)
+
+// float64
 [<Property>]
 let ``OpenBLAS.64.Mul_Dot_V_V``(v:float[]) = 
     let r = v |> Array.map (fun x -> x * x) |> Array.sum
-    (GlobalConfig.Float64Backend.Mul_Dot_V_V(v, v)) =~ r
+    Util.(=~)(Float64Backend.Mul_Dot_V_V(v, v), r)
 
 [<Property>]
 let ``OpenBLAS.64.L1Norm_V``(v:float[]) = 
     let r = v |> Array.map abs |> Array.sum
-    (GlobalConfig.Float64Backend.L1Norm_V(v)) =~ r
+    Util.(=~)(Float64Backend.L1Norm_V(v), r)
 
 [<Property>]
 let ``OpenBLAS.64.L2Norm_V``(v:float[]) = 
     let r = v |> Array.map (fun x -> x * x) |> Array.sum |> sqrt
-    (GlobalConfig.Float64Backend.L2Norm_V(v)) =~ r
+    Util.(=~)(Float64Backend.L2Norm_V(v), r)
 
 [<Property>]
 let ``OpenBLAS.64.Sum_V``(v:float[]) = 
     let r = v |> Array.sum
-    (GlobalConfig.Float64Backend.Sum_V(v)) =~ r
+    Util.(=~)(Float64Backend.Sum_V(v), r)
 
 [<Property>]
 let ``OpenBLAS.64.Sum_M``(m:float[,]) = 
@@ -61,4 +88,31 @@ let ``OpenBLAS.64.Sum_M``(m:float[,]) =
     for i = 0 to (Array2D.length1 m) - 1 do
         for j = 0 to (Array2D.length2 m) - 1 do
             r <- r + m.[i, j]
-    (GlobalConfig.Float64Backend.Sum_M(m)) =~ r
+    Util.(=~)(Float64Backend.Sum_M(m), r)
+
+[<Property>]
+let ``OpenBLAS.64.Add_V_V``(v:float[]) = 
+    let r = Array.map2 (+) v v
+    Util.(=~)(Float64Backend.Add_V_V(v, v), r)
+
+[<Property>]
+let ``OpenBLAS.64.Add_S_V``(s:float, v:float[]) = 
+    let r = v |> Array.map ((+) s)
+    Util.(=~)(Float64Backend.Add_S_V(s, v), r)
+
+[<Property>]
+let ``OpenBLAS.64.Sub_V_V``(v:float[]) = 
+    let r = v |> Array.map (fun x -> x - x)
+    Util.(=~)(Float64Backend.Sub_V_V(v, v), r)
+
+[<Property>]
+let ``OpenBLAS.64.Sub_S_V``(s:float, v:float[]) = 
+    let r = v |> Array.map (fun x -> s - x)
+    Util.(=~)(Float64Backend.Sub_S_V(s, v), r)
+
+[<Property>]
+let ``OpenBLAS.64.Sub_V_S``(v:float[], s:float) = 
+    let r = v |> Array.map (fun x -> x - s)
+    Util.(=~)(Float64Backend.Sub_V_S(v, s), r)
+
+
--- tests/DiffSharp.Tests/Tests.fs
+++ tests/DiffSharp.Tests/Tests.fs
@@ -1,32 +1,41 @@
-﻿module DiffSharp.Tests.Util
+﻿namespace DiffSharp.Tests
 
-let eps64 = 1e-4
-let eps32 = float32 eps64
+type Util() =
+    static let eps64 = 1e-4
+    static let eps32 = float32 eps64
 
-let inline (=~) a b =
-    match box a with
-    | :? float as a ->
-        match box b with
-        | :? float as b ->
-            if   System.Double.IsNaN(a) then
-                 System.Double.IsNaN(b)
-            elif System.Double.IsPositiveInfinity(a) || (a = System.Double.MaxValue) then
-                 System.Double.IsPositiveInfinity(b) || (b = System.Double.MaxValue)
-            elif System.Double.IsNegativeInfinity(a) || (a = System.Double.MinValue) then
-                 System.Double.IsNegativeInfinity(b) || (b = System.Double.MinValue)
-            else 
-                 abs (a - b) < eps64
-        | _ -> false
-    | :? float32 as a ->
-        match box b with
-        | :? float32 as b ->
-            if  System.Single.IsNaN(a) then 
-                System.Single.IsNaN(b)
-            elif System.Single.IsPositiveInfinity(a) || (a = System.Single.MaxValue) then
-                 System.Single.IsPositiveInfinity(b) || (b = System.Single.MaxValue)
-            elif System.Single.IsNegativeInfinity(a) || (a = System.Single.MinValue) then
-                 System.Single.IsNegativeInfinity(b) || (b = System.Single.MinValue)
-            else 
-                abs (a - b) < eps32
-        | _ -> false
-    | _ -> false
+    static member (=~) (a:float32, b:float32) =
+        if   System.Single.IsNaN(a) then
+             System.Single.IsNaN(b)
+        elif System.Single.IsPositiveInfinity(a) || (a = System.Single.MaxValue) then
+             System.Single.IsPositiveInfinity(b) || (b = System.Single.MaxValue)
+        elif System.Single.IsNegativeInfinity(a) || (a = System.Single.MinValue) then
+             System.Single.IsNegativeInfinity(b) || (b = System.Single.MinValue)
+        else 
+             abs (a - b) < eps32
+
+    static member (=~) (a:float, b:float) =
+        if   System.Double.IsNaN(a) then
+             System.Double.IsNaN(b)
+        elif System.Double.IsPositiveInfinity(a) || (a = System.Double.MaxValue) then
+             System.Double.IsPositiveInfinity(b) || (b = System.Double.MaxValue)
+        elif System.Double.IsNegativeInfinity(a) || (a = System.Double.MinValue) then
+             System.Double.IsNegativeInfinity(b) || (b = System.Double.MinValue)
+        else 
+             abs (a - b) < eps64
+
+    static member (=~) (a:float32[], b:float32[]) =
+        if a.Length <> b.Length then
+            false
+        else
+            match Array.map2 (fun (x:float32) (y:float32) -> (Util.(=~)(x, y))) a b |> Array.tryFind not with
+            | Some(_) -> false
+            | _ -> true
+
+    static member (=~) (a:float[], b:float[]) =
+        if a.Length <> b.Length then
+            false
+        else
+            match Array.map2 (fun (x:float) (y:float) -> (Util.(=~)(x, y))) a b |> Array.tryFind not with
+            | Some(_) -> false
+            | _ -> true
