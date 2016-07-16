620010315070283b51a72203d5000b42a5f9f767
Gauthier Segay <gauthier.segay@gmail.com>
2016-07-16 18:39:59 +0200

    add appveyor descriptor and build.cmd script

---
+++ build.cmd
@@ -0,0 +1,13 @@
+@echo off
+cls
+
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+msbuild DiffSharp.sln
