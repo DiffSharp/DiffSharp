061fe77319d0e91d8ba194beda73231994ef8e90
dsyme <dsyme@MSRC-4079076>
2017-07-19 18:51:34 +0100

    fix bug 27

---
+++ tests/DiffSharp.Tests/Symbolic.fs
@@ -0,0 +1,21 @@
+﻿module Symbolic
+
+open FsCheck.NUnit
+open DiffSharp.Util
+open DiffSharp.Tests
+open DiffSharp.Symbolic.Float64
+
+[<ReflectedDefinition>]
+let f2 x = x**2.0
+
+[<Property>]
+// See https://github.com/DiffSharp/DiffSharp/issues/27
+let ``Diff.Of.Exponential.At.Zero``() = 
+    let dy = diff <@ f2 @>
+    Util.(=~) (dy 0.0, 0.0)
+
+[<Property>]
+let ``Diff.Of.Exponential.At.One``() = 
+    let dy = diff <@ f2 @>
+    Util.(=~) (dy 1.0, 2.0)
+
