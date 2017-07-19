185fe437a5748a529c64dfff04a6824281bb6d54
dsyme <dsyme@MSRC-4079076>
2017-07-19 18:55:57 +0100

    fix bug 27

--- tests/DiffSharp.Tests/Symbolic.fs
+++ tests/DiffSharp.Tests/Symbolic.fs
@@ -3,36 +3,39 @@
 open FsCheck.NUnit
 open DiffSharp.Util
 open DiffSharp.Tests
+open NUnit.Framework
 
 module Float64 = 
     open DiffSharp.Symbolic.Float64
     [<ReflectedDefinition>]
     let f2 x = x**2.0
 
-    [<Property>]
+    [<Test>]
     // See https://github.com/DiffSharp/DiffSharp/issues/27
     let ``Diff.Of.Exponential.At.Zero``() = 
         let dy = diff <@ f2 @>
-        Util.(=~) (dy 0.0, 0.0)
+        Assert.True( Util.(=~) (dy 0.0, 0.0))
 
-    [<Property>]
+    [<Test>]
     let ``Diff.Of.Exponential.At.One``() = 
         let dy = diff <@ f2 @>
-        Util.(=~) (dy 1.0, 2.0)
+        Assert.True(Util.(=~) (dy 1.0, 2.0))
 
 module Float32 = 
     open DiffSharp.Symbolic.Float32
+
     [<ReflectedDefinition>]
     let f2 x = x**2.0f
 
-    [<Property>]
+    [<Test>]
     // See https://github.com/DiffSharp/DiffSharp/issues/27
     let ``Diff.Of.Exponential.At.Zero``() = 
+        printfn "testing"
         let dy = diff <@ f2 @>
-        Util.(=~) (dy 0.0f, 0.0f)
+        Assert.True(Util.(=~) (dy 0.0f, 0.0f))
 
-    [<Property>]
+    [<Test>]
     let ``Diff.Of.Exponential.At.One``() = 
         let dy = diff <@ f2 @>
-        Util.(=~) (dy 1.0f, 2.0f)
+        Assert.True(Util.(=~) (dy 1.0f, 2.0f))
 
