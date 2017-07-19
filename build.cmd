b5be5cd3fafccaaea97aa6ef2ad13934a0b8fdde
dsyme <dsyme@MSRC-4079076>
2017-07-19 17:30:38 +0100

    fix linux tests

--- build.cmd
+++ build.cmd
@@ -10,5 +10,5 @@ if errorlevel 1 (
 if errorlevel 1 (
   exit /b %errorlevel%
 )
-msbuild DiffSharp.sln /p:Configuration=Release 
-packages\NUnit.Runners\tools\nunit-console "tests\DiffSharp.Tests\bin\Release\DiffSharp.Tests.dll" 
+msbuild DiffSharp.sln /p:Configuration=Debug 
+packages\NUnit.Runners\tools\nunit-console "tests\DiffSharp.Tests\bin\Debug\DiffSharp.Tests.dll" 
