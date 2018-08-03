20f69a4c98c88025866cff79516dd65dee5a39e0
Don Syme <dsyme@microsoft.com>
2018-08-03 23:11:40 +0100

    try build mono

--- build.sh
+++ build.sh
@@ -20,8 +20,15 @@
 sudo apt-get -y update
 sudo apt-get -y install libopenblas-dev
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib/OpenBLAS-v0.2.14-Linux64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/OpenBLAS-v0.2.8-Linux64;/usr/local/lib/OpenBLAS-v0.2.8-Linux64
 
-dotnet build DiffSharp.sln -c debug 
-dotnet test tests/DiffSharp.Tests  -c debug 
+if [ ! -d "MONO" ]; then
+   msbuild DiffSharp.sln /p:Configuration=Release
+   msbuild /p:Configuration=Release DiffSharp.sln
+   # not currently testing on mono
+   # mono ./packages/NUnit.Runners/tools/nunit-console.exe ./tests/DiffSharp.Tests/bin/Release/DiffSharp.Tests.dll
+else
+   dotnet build DiffSharp.sln -c debug 
+   dotnet test tests/DiffSharp.Tests  -c release
+fi
 
