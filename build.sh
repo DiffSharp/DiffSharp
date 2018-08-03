444070eb6d66c9379c6226fedf98197c48bdf16d
Don Syme <dsyme@microsoft.com>
2018-08-03 23:57:59 +0100

    try fix osx

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-# next part copied from (check there for newest version): 
-# https://github.com/deeplearningparis/dl-machine/blob/master/scripts/install-deeplearning-libraries.sh
-# Build latest stable release of OpenBLAS without OPENMP to make it possible
-# to use Python multiprocessing and forks without crash
-# The torch install script will install OpenBLAS with OPENMP enabled in
-# /opt/OpenBLAS so we need to install the OpenBLAS used by Python in a
-# distinct folder.
-# Note: the master branch only has the release tags in it
-#sudo apt-get install -y gfortran
-#export OPENBLAS_ROOT=/opt/OpenBLAS-no-openmp
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENBLAS_ROOT/lib
-#if [ ! -d "OpenBLAS" ]; then
-#    git clone -q --branch=master git://github.com/xianyi/OpenBLAS.git
-#    (cd OpenBLAS && make FC=gfortran USE_OPENMP=0 NO_AFFINITY=1 NUM_THREADS=4 && sudo make install PREFIX=$OPENBLAS_ROOT)
-#    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> ~/.bashrc
-#fi
-#sudo ldconfig
 
-sudo apt-get -y update
-sudo apt-get -y install libopenblas-dev
+case "$(uname -s)" in
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/OpenBLAS-v0.2.8-Linux64;/usr/local/lib/OpenBLAS-v0.2.8-Linux64
+   Darwin)
+     brew install homebrew/science/openblas
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*|Linux)
+    echo 'Linux, Cygwin, etc' 
+    sudo apt-get -y update
+    sudo apt-get -y install libopenblas-dev
+     ;;
+
+   # Add here more strings to compare
+   # See correspondence table at the bottom of this answer
+
+   *)
+     echo 'other OS' 
+     ;;
+esac
+
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/OpenBLAS-v0.2.8-Linux64;/usr/local/lib/OpenBLAS-v0.2.8-Linux64
 
 if [ -d "MONO" ]; then
    msbuild DiffSharp.sln /p:Configuration=Release
