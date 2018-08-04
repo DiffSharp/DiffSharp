146d94bc0cdebc119634ed0e3280eff6b55d0356
Don Syme <dsyme@microsoft.com>
2018-08-04 01:47:33 +0100

    turn test discovery off on appveyor

--- build.cmd
+++ build.cmd
@@ -1,8 +1,5 @@
-@echo off
 cls
 
-REM dotnet --version
 dotnet --version
-
 dotnet build DiffSharp.sln -c release -v:n
-dotnet test tests/DiffSharp.Tests  -c release -f netcoreapp2.0 -v:n
+dotnet test tests/DiffSharp.Tests  -c release -v:n
