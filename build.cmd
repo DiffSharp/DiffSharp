ec657b170189ae77c6d4ec31eb3dc11ece48a67d
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-04-16 12:42:59 +0100

    Initial code

--- build.cmd
+++
@@ -1,5 +0,0 @@
-cls
-
-dotnet --version
-dotnet build DiffSharp.sln -c release -v:n
-dotnet test tests/DiffSharp.Tests  -c release -v:n
---
+++ src/Tests/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open DiffSharp
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let t = Tensor.Create(2.f)
+    printfn "%A" t
+    0 // return an integer exit code
