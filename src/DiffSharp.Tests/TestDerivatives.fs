f6aed002e49ee89893b654457d6ea18806790742
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-09-18 16:59:23 +0100

    SubTT test

--- src/DiffSharp.Tests/TestDerivatives.fs
+++ src/DiffSharp.Tests/TestDerivatives.fs
@@ -25,9 +25,34 @@ type TestDerivatives () =
         let revzCorrect = Tensor.Create([6.; 8.; 10.])
         revz.Reverse()
         let revxd = revx.Derivative
-        let revxdCorrect = Tensor.Create([1.;1.;1.])
+        let revxdCorrect = Tensor.Create([1.; 1.; 1.])
         let revyd = revy.Derivative
-        let revydCorrect = Tensor.Create([1.;1.;1.])
+        let revydCorrect = Tensor.Create([1.; 1.; 1.])
+
+        Assert.AreEqual(fwdz, fwdzCorrect)
+        Assert.AreEqual(fwdzd, fwdzdCorrect)
+        Assert.AreEqual(revz, revzCorrect)
+        Assert.AreEqual(revxd, revxdCorrect)
+        Assert.AreEqual(revyd, revydCorrect)
+
+    [<Test>]
+    member this.TestDerivativeSubTT () =
+        let fwdx = Tensor.Create([1.; 2.; 3.]).GetForward(Tensor.Create([2.; 3.; 4.]), 1u)
+        let fwdy = Tensor.Create([5.; 6.; 7.]).GetForward(Tensor.Create([2.; 2.; 3.]), 1u)
+        let fwdz = fwdx - fwdy
+        let fwdzCorrect = Tensor.Create([-4.; -4.; -4.])
+        let fwdzd = fwdz.Derivative
+        let fwdzdCorrect = Tensor.Create([0.; 1.; 1.])
+
+        let revx = Tensor.Create([1.; 2.; 3.]).GetReverse(1u)
+        let revy = Tensor.Create([5.; 6.; 7.]).GetReverse(1u)
+        let revz = revx - revy
+        let revzCorrect = Tensor.Create([-4.; -4.; -4.])
+        revz.Reverse()
+        let revxd = revx.Derivative
+        let revxdCorrect = Tensor.Create([1.; 1.; 1.])
+        let revyd = revy.Derivative
+        let revydCorrect = Tensor.Create([-1.; -1.; -1.])
 
         Assert.AreEqual(fwdz, fwdzCorrect)
         Assert.AreEqual(fwdzd, fwdzdCorrect)
