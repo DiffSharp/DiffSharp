fe7a8bb81e2522adfc7f99010d501800b29a5a66
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-09-21 02:48:16 +0100

    Matmul derivative test

--- src/DiffSharp.Tests/TestDerivatives.fs
+++ src/DiffSharp.Tests/TestDerivatives.fs
@@ -166,6 +166,69 @@ type TestDerivatives () =
     // TODO: add test for DivTConstT0
 
     [<Test>]
+    member this.TestDerivativeMatMulT2T2 () =
+        let fwdx = Tensor.Create([[6.2381; 0.0393; 8.2364; 3.9906; 6.2291];
+            [9.8762; 3.2263; 6.2866; 4.7111; 0.0652];
+            [3.5832; 7.9801; 1.9854; 4.4965; 4.1712]])
+        let fwdx = fwdx.GetForward(Tensor.Create([[4.6453; 8.4388; 4.6549; 9.5680; 1.5756];
+            [3.2066; 4.2429; 2.2028; 9.1037; 3.4022];
+            [4.2324; 4.5508; 3.4755; 2.7196; 5.5344]]), 1u)
+        let fwdy = Tensor.Create([[4.4220; 3.7293];
+            [6.1928; 2.1446];
+            [0.0525; 1.2494];
+            [7.5281; 1.4816];
+            [5.0328; 2.2756]])
+        let fwdy = fwdy.GetForward(Tensor.Create([[1.4749; 9.7608];
+            [3.6599; 7.9553];
+            [3.5503; 1.3757];
+            [8.3172; 6.6748];
+            [2.2959; 0.6784]]), 1u)
+        let fwdz = Tensor.MatMul(fwdx, fwdy)
+        let fwdzCorrect = Tensor.Create([[ 89.6516; 53.7260];
+            [ 99.7751; 58.7331];
+            [120.2113; 49.1116]])
+        let fwdzd = fwdz.Derivative
+        let fwdzdCorrect = Tensor.Create([[239.0819; 162.3930];
+            [214.2522; 207.2430];
+            [183.9220; 180.5424]])
+
+        let revx = Tensor.Create([[6.2381; 0.0393; 8.2364; 3.9906; 6.2291];
+            [9.8762; 3.2263; 6.2866; 4.7111; 0.0652];
+            [3.5832; 7.9801; 1.9854; 4.4965; 4.1712]]).GetReverse(1u)
+        let revy = Tensor.Create([[4.4220; 3.7293];
+            [6.1928; 2.1446];
+            [0.0525; 1.2494];
+            [7.5281; 1.4816];
+            [5.0328; 2.2756]]).GetReverse(1u)
+        let revz = Tensor.MatMul(revx, revy)
+        let revzCorrect = Tensor.Create([0.2; 0.333333; 0.428571])
+        let revzCorrect = Tensor.Create([[ 89.6516; 53.7260];
+            [ 99.7751; 58.7331];
+            [120.2113; 49.1116]])
+        revz.Reverse(Tensor.Create([[7.3984; 0.1849];
+            [1.2520; 9.5731];
+            [6.8201; 9.5221]]))            
+        let revxd = revx.Derivative
+        let revxdCorrect = Tensor.Create([[33.4050; 46.2136;  0.6191; 55.9696; 37.6556];
+            [41.2370; 28.2842; 12.0266; 23.6085; 28.0854];
+            [65.6689; 62.6571; 12.2551; 65.4497; 55.9926]])
+        let revyd = revy.Derivative
+        let revydCorrect = Tensor.Create([[ 82.9549;129.8180];
+            [ 58.7551;106.8801];
+            [ 82.3474; 80.6097];
+            [ 66.0888; 88.6534];
+            [ 74.6154; 41.4950]])
+
+        Assert.True(fwdz.ApproximatelyEqual(fwdzCorrect))
+        Assert.True(fwdzd.ApproximatelyEqual(fwdzdCorrect))
+        Assert.True(revz.ApproximatelyEqual(revzCorrect))
+        Assert.True(revxd.ApproximatelyEqual(revxdCorrect))
+        Assert.True(revyd.ApproximatelyEqual(revydCorrect))
+
+    //TODO: add test for MatMulT2T2Const
+    //TODO: add test for MatMulT2ConstT2
+
+    [<Test>]
     member this.TestDerivativeNeg () =
         let fwdx = Tensor.Create([1.; 2.; 3.]).GetForward(Tensor.Create([2.; 3.; 4.]), 1u)
         let fwdz = -fwdx
