8d8bf172532d13eb60615f31f47ef6b617c0ce68
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-11-05 16:01:10 +0000

    Optimizer

--- src/DiffSharp/Optim.fs
+++ src/DiffSharp/Optim.fs
@@ -3,5 +3,11 @@ open DiffSharp
 open DiffSharp.Model
 
 [<AbstractClass>]
-type Optimizer() =
-    abstract member Step : unit -> unit
+type Optimizer(model:Model) =
+    member val Model = model
+    abstract member ParameterUpdate : Parameter -> unit
+    member o.Step() = o.Model.IterParameters(fun _ p -> o.ParameterUpdate(p))
+
+type SGD(model, lr:float) =
+    inherit Optimizer(model)
+    override o.ParameterUpdate(p) = p.Tensor <- p.Tensor.Primal - lr * p.Tensor.Derivative
