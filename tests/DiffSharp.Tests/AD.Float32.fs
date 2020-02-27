8dce4da2dfaf77afc288aa6ecbb4efbcd19c59f1
Colin Gravill <cgravill@users.noreply.github.com>
2020-02-27 13:28:10 +0000

    Disabled some tests that are no longer compatible with DiffSharp

--- tests/DiffSharp.Tests/AD.Float32.fs
+++ tests/DiffSharp.Tests/AD.Float32.fs
@@ -12,8 +12,7 @@ open DiffSharp.Util
 open DiffSharp.Tests
 open DiffSharp.AD.Float32
 
-
-[<Property>]
+(*[<Property>]
 let ``AD.32.F.D.FixedPoint``() =
     let g (a:D) (b:D) = (a + b / a) / (D 2.f)
     let p, t = jacobianv' (D.FixedPoint g (D 1.2f)) (D 25.f) (D 1.f)
@@ -23,7 +22,7 @@ let ``AD.32.F.D.FixedPoint``() =
 let ``AD.32.R.D.FixedPoint``() =
     let g (a:D) (b:D) = (a + b / a) / (D 2.f)
     let p, t = jacobianTv' (D.FixedPoint g (D 1.2f)) (D 25.f) (D 1.f)
-    Util.(=~)(p, D 5.f) && Util.(=~)(t, D 0.1f)
+    Util.(=~)(p, D 5.f) && Util.(=~)(t, D 0.1f)*)
 
 [<Property>]
 let ``Compute Adjoint``() =
