fc161701ff160ee395635b94b263a07f299a89c4
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2020-05-21 13:38:16 +0100

    adam working

--- src/DiffSharp.Core/Optim.fs
+++ src/DiffSharp.Core/Optim.fs
@@ -76,7 +76,7 @@ type Optimizer(model:Model) =
         let printPrefix = defaultArg printPrefix ""
         let printPostfix = defaultArg printPostfix ""
         let printNewLine = defaultArg printNewLine true
-        let mutable printEnd = if printNewLine then "\n" else "                    \r"
+        let mutable printEnd = ""
         let mutable status = ""
         let mutable epoch = -1
         let mutable i = -1
@@ -98,20 +98,25 @@ type Optimizer(model:Model) =
                 
                 let lScalar = float l
 
+                printEnd <- if printNewLine then "\n" else "                    \r"
                 if l.hasnan() || l.hasinf() then
                     status <- "Diverged"
                     stop <- true
                 elif thresholdGiven && lScalar <= threshold then
                     status <- sprintf "Converged (loss < %g)" threshold
+                    printEnd <- "\n"
                     stop <- true
                 elif i=iters-1 then
                     status <- sprintf "Iters=%d reached" iters
+                    printEnd <- "\n"
                     stop <- true
                 elif lScalar < lMin then
                     lMin <- lScalar
+                    printEnd <- "\n"
                     status <- "🡾 New min"
                 elif lScalar > lMax then
                     lMax <- lScalar
+                    printEnd <- "\n"
                     status <- "🡽 New max"
                 elif lScalar < lPrev then
                     status <- "🡾"
@@ -119,7 +124,6 @@ type Optimizer(model:Model) =
                     status <- "🡽"
 
                 if print && ((i+1) % printEvery = 0 || i = 0 || stop) then
-                    if stop then printEnd <- "\n"
                     printf "%s%4d | %d | %d/%d | %e %s%s%s" printPrefix (i+1) (epoch+1) (bi+1) dataloader.length lScalar status printPostfix printEnd
                 lPrev <- lScalar
                 not stop
@@ -144,21 +148,46 @@ type Optimizer(model:Model) =
             f, x - lr * p
         Optimizer.optimizeFun(update, x0, ?iters=iters, ?threshold=threshold, ?print=print, ?printEvery=printEvery, ?printPrefix=printPrefix, ?printPostfix=printPostfix, ?printNewLine=printNewLine)
 
+    static member adam(f, x0:Tensor, ?lr:Tensor, ?beta1:Tensor, ?beta2:Tensor, ?eps:Tensor, ?iters:int, ?threshold:double, ?print:bool, ?printEvery:int, ?printPrefix:string, ?printPostfix:string, ?printNewLine:bool) =
+        let lr = defaultArg lr (dsharp.tensor(1e-3))
+        let beta1 = defaultArg beta1 (dsharp.tensor(0.9))
+        let beta2 = defaultArg beta2 (dsharp.tensor(0.999))
+        let eps = defaultArg eps (dsharp.tensor(1e-8))
+        let mutable step = 0
+        let mutable expAvg = x0.zerosLike()
+        let mutable expAvgSq = x0.zerosLike()
+        let update x =
+            let f, g = dsharp.fg f x
+            step <- step + 1
+            expAvg <- expAvg.mul(beta1).add(g*(1.-beta1))
+            expAvgSq <- expAvgSq.mul(beta2).add(g*g*(1.-beta2))
+            let biasCorrection1 = 1. - beta1 ** step
+            let biasCorrection2 = 1. - beta2 ** step
+            let denom = (expAvgSq.sqrt() / biasCorrection2.sqrt()).add(eps)
+            let p = expAvg / denom
+            let stepSize = lr / biasCorrection1
+            f, x - stepSize * p
+        Optimizer.optimizeFun(update, x0, ?iters=iters, ?threshold=threshold, ?print=print, ?printEvery=printEvery, ?printPrefix=printPrefix, ?printPostfix=printPostfix, ?printNewLine=printNewLine)
+
     static member sgd(model, dataloader, loss, ?lr:Tensor, ?momentum:Tensor, ?nesterov:bool, ?weightDecay:Tensor, ?reversible:bool, ?iters:int, ?threshold:double, ?print:bool, ?printEvery:int, ?printPrefix:string, ?printPostfix:string, ?printNewLine:bool) =
         let optimizer = SGD(model, ?lr=lr, ?momentum=momentum, ?nesterov=nesterov, ?weightDecay=weightDecay, ?reversible=reversible)
         Optimizer.optimizeModel(model, optimizer, dataloader, loss, ?iters=iters, ?threshold=threshold, ?print=print, ?printEvery=printEvery, ?printPrefix=printPrefix, ?printPostfix=printPostfix, ?printNewLine=printNewLine)
 
+    static member adam(model, dataloader, loss, ?lr:Tensor, ?beta1:Tensor, ?beta2:Tensor, ?eps:Tensor, ?weightDecay:Tensor, ?reversible:bool, ?iters:int, ?threshold:double, ?print:bool, ?printEvery:int, ?printPrefix:string, ?printPostfix:string, ?printNewLine:bool) =
+        let optimizer = Adam(model, ?lr=lr, ?beta1=beta1, ?beta2=beta2, ?eps=eps, ?weightDecay=weightDecay, ?reversible=reversible)
+        Optimizer.optimizeModel(model, optimizer, dataloader, loss, ?iters=iters, ?threshold=threshold, ?print=print, ?printEvery=printEvery, ?printPrefix=printPrefix, ?printPostfix=printPostfix, ?printNewLine=printNewLine)
 
 
 and SGD(model, ?lr:Tensor, ?momentum:Tensor, ?nesterov:bool, ?weightDecay:Tensor, ?reversible:bool) =
     inherit Optimizer(model)
-    let lr = defaultArg lr (dsharp.tensor(0.001))
+    let lr = defaultArg lr (dsharp.tensor(1e-3))
     let nesterov = defaultArg nesterov true
     let reversible = defaultArg reversible false
-    let mutable momBuffer = ParameterDict()
     let mutable momInit = false
+    let mutable momBuffer = ParameterDict()
     override o.updateRule name t = 
         let mutable d = t.derivative
+        let t = if reversible then t else t.primal
         match weightDecay with
         | Some wd -> d <- d.add(t.primal * wd)
         | None -> ()
@@ -173,10 +202,7 @@ and SGD(model, ?lr:Tensor, ?momentum:Tensor, ?nesterov:bool, ?weightDecay:Tensor
             if nesterov then d <- d.add(mb*mom)
             else d <- mb
         | None -> ()   
-        if reversible then
-            t - lr * d
-        else
-            t.primal - lr * d
+        t - lr * d
 
 
 and Adam(model, ?lr:Tensor, ?beta1:Tensor, ?beta2:Tensor, ?eps:Tensor, ?weightDecay:Tensor, ?reversible:bool) =
@@ -185,20 +211,26 @@ and Adam(model, ?lr:Tensor, ?beta1:Tensor, ?beta2:Tensor, ?eps:Tensor, ?weightDe
     let beta1 = defaultArg beta1 (dsharp.tensor(0.9))
     let beta2 = defaultArg beta2 (dsharp.tensor(0.999))
     let eps = defaultArg eps (dsharp.tensor(1e-8))
-    let reversible = defaultArg reversible false    
-    let mutable stateStep = -1
-    let mutable stateExpAvg = dsharp.zero()
-    let mutable stateExpAvgSq = dsharp.zero()
+    let reversible = defaultArg reversible false
+    let mutable stateStep = 0
+    let mutable stateExpAvg = ParameterDict()
+    let mutable stateExpAvgSq = ParameterDict()
     override o.updateRule name t =
         let mutable d = t.derivative
+        let t = if reversible then t else t.primal
         match weightDecay with
         | Some wd -> d <- d.add(t.primal * wd)
         | None -> ()
-        if stateStep = -1 then
-            stateExpAvg <- t.zerosLike()
-
+        if stateStep = 0 then
+            stateExpAvg <- model.Parameters.map(fun (t:Tensor) -> t.zerosLike())
+            stateExpAvgSq <- model.Parameters.map(fun (t:Tensor) -> t.zerosLike())
         stateStep <- stateStep + 1
-        if reversible then
-            t - lr * d
-        else
-            t.primal - lr * d
+        let expAvg = stateExpAvg.[name].mul(beta1).add(d*(1.-beta1))
+        let expAvgSq = stateExpAvgSq.[name].mul(beta2).add(d*d*(1.-beta2))
+        stateExpAvg.[name] <- expAvg
+        stateExpAvgSq.[name] <- expAvgSq
+        let biasCorrection1 = 1. - beta1 ** stateStep
+        let biasCorrection2 = 1. - beta2 ** stateStep
+        let denom = (expAvgSq.sqrt() / biasCorrection2.sqrt()).add(eps)
+        let stepSize = lr / biasCorrection1
+        t - stepSize * (expAvg/denom)
