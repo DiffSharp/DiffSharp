6b0c5616f9e3c1b2c65baf6ebfe8ef4b66a52c72
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2020-06-07 12:13:08 +0100

    isolate

--- tests/MemoryTest/Program.fs
+++ tests/MemoryTest/Program.fs
@@ -1,17 +1,19 @@
 ﻿open System
 open DiffSharp
+//open TorchSharp
 
 [<EntryPoint>]
 let main _ =
-    //dsharp.config(backend=Backend.Reference)
-    dsharp.config(backend=Backend.Torch)
-    dsharp.seed(1)
+    //dsharp.config(backend=Backend.Reference) // Uses around 23 MB
+    dsharp.config(backend=Backend.Torch) // Uses around 4GB
 
     printfn "Press any key to continue..."
     Console.ReadKey() |> ignore
 
     for i in 0..1024 do
-        let _ = dsharp.randn([1024; 1024])
+        let x = dsharp.zeros([1024; 1024])
+        //let x = TorchSharp.Tensor.FloatTensor.Zeros([|1024L;1024L|])
+        //x.Dispose()
         printfn "%A" i
         System.GC.Collect()
 
