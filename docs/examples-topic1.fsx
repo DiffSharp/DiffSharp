3d1005810c2bc1d8112cdcb66130b8d4ca9cfd7d
Don Syme <dsyme@microsoft.com>
2020-09-07 17:48:04 +0100

    doc updates

--- docs/examples-topic1.fsx
+++ docs/examples-topic1.fsx
@@ -1,6 +1,7 @@
 ﻿(*** condition: prepare ***)
-#r "../src/DiffSharp.Core/bin/Debug/netstandard2.1/DiffSharp.Core.dll"
-#r "../src/DiffSharp.Backends.Reference/bin/Debug/netstandard2.1/DiffSharp.Backends.Reference.dll"
+#I "../tests/DiffSharp.Tests/bin/Debug/netcoreapp3.0"
+#r "DiffSharp.Core.dll"
+#r "DiffSharp.Backends.Reference.dll"
 (*** condition: fsx ***)
 #if FSX
 #r "nuget:RestoreSources=https://ci.appveyor.com/nuget/diffsharp"
