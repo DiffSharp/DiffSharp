7676e3c08511c4326dbe836c4db024847b94189c
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2021-02-13 02:23:43 +0000

    DiffSharp.IO

--- src/DiffSharp.Core/Tensor.fs
+++ src/DiffSharp.Core/Tensor.fs
@@ -746,43 +746,6 @@ type Tensor =
             sb.AppendLine() |> ignore
         sb.ToString()
 
-    /// <summary>Save tensor to an image file using png or jpg format</summary>
-    member t.saveImage(fileName:string, ?pixelMin:double, ?pixelMax:double, ?normalize:bool, ?gridCols:int) =
-        let pixels:Tensor = t.toImage(?pixelMin=pixelMin, ?pixelMax=pixelMax, ?normalize=normalize, ?gridCols=gridCols)
-        let c, h, w = pixels.shape.[0], pixels.shape.[1], pixels.shape.[2]        
-        let image = new SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.RgbaVector>(w, h)
-        for y=0 to h-1 do
-            for x=0 to w-1 do
-                let r, g, b = 
-                    if c = 1 then
-                        let v = float32(pixels.[0, y, x])
-                        v, v, v
-                    else
-                        float32(pixels.[0, y, x]), float32(pixels.[1, y, x]), float32(pixels.[2, y, x])
-                image.Item(x, y) <- SixLabors.ImageSharp.PixelFormats.RgbaVector(r, g, b)
-        let fs = new System.IO.FileStream(fileName, System.IO.FileMode.Create)
-        let encoder =
-            if fileName.EndsWith(".jpg") then
-                SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder() :> SixLabors.ImageSharp.Formats.IImageEncoder
-            elif fileName.EndsWith(".png") then
-                SixLabors.ImageSharp.Formats.Png.PngEncoder() :> SixLabors.ImageSharp.Formats.IImageEncoder
-            else
-                failwithf "Expecting fileName (%A) to end with .png or .jpg" fileName
-        image.Save(fs, encoder)
-        fs.Close()
-
-    /// <summary>Load an image file and return it as a tensor</summary>
-    static member loadImage(fileName:string, ?normalize:bool, ?dtype: Dtype, ?device: Device, ?backend: Backend) =
-        let normalize = defaultArg normalize false
-        let image:SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.RgbaVector> = SixLabors.ImageSharp.Image.Load(fileName)
-        let pixels = Array3D.init 3 image.Height image.Width (fun c y x -> let p = image.Item(x, y)
-                                                                           if c = 0 then p.R
-                                                                           elif c = 1 then p.G
-                                                                           else p.B)
-        let mutable pixels:Tensor = Tensor.create(pixels, ?dtype=dtype, ?device=device, ?backend=backend)
-        if normalize then pixels <- pixels.normalize()
-        pixels
-
     member internal t.GetSlice(bounds:int[,]) =
         // printfn "t.GetSlice bounds\n %A" bounds
         if t.dim = 0 then failwith "Cannot slice a scalar Tensor"
