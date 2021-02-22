e3bb9b826990b767d850d70cd419ed771bf5fbd4
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2021-02-22 02:52:16 +0000

    params naming, init

--- src/DiffSharp.Core/Model.fs
+++ src/DiffSharp.Core/Model.fs
@@ -37,7 +37,7 @@ type Parameter =
 
 
 /// <summary>Represents a collection of named parameters in a model.</summary>
-type ParametersDict() =
+type ParameterDict() =
 
     // If the dictionary is empty then the latest 'move' is considered the configuration for the implied empty tensor
     let mutable dummy = dsharp.zeros(0)
@@ -57,14 +57,14 @@ type ParametersDict() =
     member d.add(parameters:list<string*Parameter>) = for (n, p) in parameters do d.add(n, p)
 
     /// <summary>TBD</summary>
-    member d.add(parameters:ParametersDict) = for KeyValue(n, p) in parameters.values do d.add(n, p)
+    member d.add(parameters:ParameterDict) = for KeyValue(n, p) in parameters.values do d.add(n, p)
 
     /// <summary>TBD</summary>
     member d.copy() = d.map(fun (t:Tensor) -> t)
 
     /// <summary>TBD</summary>
     member d.map(f:string*Parameter->string*Parameter) =
-        let ret = ParametersDict()
+        let ret = ParameterDict()
         for KeyValue(n, p) in d.values do ret.values.Add(f(n,p))
         ret
 
@@ -75,13 +75,13 @@ type ParametersDict() =
     member d.map(f:Tensor->Tensor) = d.map(fun (n,t) -> n, f t)
 
     /// <summary>TBD</summary>
-    member d.set(parameters:ParametersDict) = d.iter(fun (n, p) -> p.value <- parameters.[n])
+    member d.set(parameters:ParameterDict) = d.iter(fun (n, p) -> p.value <- parameters.[n])
 
     /// <summary>TBD</summary>
     member d.iter(f:string*Parameter->unit) = for KeyValue(n, p) in d.values do f(n,p)
 
     /// <summary>TBD</summary>
-    member d.forwarddiff(derivatives:ParametersDict, ?tag:uint32) = 
+    member d.forwardDiff(derivatives:ParameterDict, ?tag:uint32) = 
         let tag = defaultArg tag GlobalNestingLevel.Current
         d.iter(fun (n, p) -> p.forwardDiff(derivatives.[n], tag))
 
@@ -116,7 +116,7 @@ type ParametersDict() =
     /// <summary>TBD</summary>
     member d.unflatten(tensors:Tensor) =
         if tensors.dim <> 1 then failwithf "Expecting 1d tensors but received tensors with shape %A" tensors.shape
-        if tensors.nelement <> d.nelement then failwithf "Expecting tensors.nelement (%A) and ParametersDict.nelement (%A) to be the same" tensors.nelement d.nelement
+        if tensors.nelement <> d.nelement then failwithf "Expecting tensors.nelement (%A) and ParameterDict.nelement (%A) to be the same" tensors.nelement d.nelement
         let shapes = [|for t in d.values.Values do t.value.shape|]
         let sizes = [|for s in shapes do shapeLength s|]
         let ts = Array.map2 (fun (t:Tensor) (s:int[]) -> t.view(s)) (tensors.split(sizes)) shapes
@@ -135,7 +135,7 @@ type ParametersDict() =
     /// <summary>TBD</summary>
     override d.ToString() =
         let sb = System.Text.StringBuilder()
-        sb.Append("ParametersDict(") |> ignore
+        sb.Append("ParameterDict(") |> ignore
         let mutable prefix = ""
         for KeyValue(n, p) in d.values do 
             sb.Append(sprintf "%s%A:%A" prefix n p) |> ignore
@@ -156,10 +156,10 @@ type Model() =
     val mutable mode: Mode
 
     /// <summary>TBD</summary>
-    member val ParametersDict = ParametersDict()
+    let parameterDict = ParameterDict()
 
     /// <summary>TBD</summary>
-    member val SubModelsDict = Dictionary<string, Model>()
+    member val subModels = Dictionary<string, Model>()
 
     /// <summary>TBD</summary>
     member m.train() = 
@@ -172,20 +172,23 @@ type Model() =
         for model:Model in m.allModels do model.mode <- Mode.Eval
 
     /// <summary>TBD</summary>
-    member m.parametersDict
-        with get () = m.ParametersDict
-        and set parameters = m.ParametersDict.set(parameters)
+    member m.parameters
+        with get () = parameterDict
+        and set parameters = parameterDict.set(parameters)
 
     /// <summary>TBD</summary>
-    member m.parameters
-        with get () = m.parametersDict.flatten()
-        and set parameters = m.parametersDict.unflatten(parameters)
+    member m.parametersVector
+        with get () = m.parameters.flatten()
+        and set parameters = m.parameters.unflatten(parameters)
 
     /// <summary>TBD</summary>
     member m.allModels
         with get () =
-            if m.SubModelsDict.Count = 0 then [m]
-            else [for sm in m.SubModelsDict.Values do yield! sm.allModels]
+            if m.subModels.Count = 0 then [m]
+            else [for sm in m.subModels.Values do yield! sm.allModels]
+
+    /// <summary>TBD</summary>
+    member m.init(f:string*Tensor->Tensor) = for KeyValue(n, p) in m.parameters.values do p.value <- f(n, p.value)
 
     /// <summary>TBD</summary>
     member m.add(parameters:seq<obj>, ?names:seq<string>) =
@@ -195,33 +198,33 @@ type Model() =
         for p, n in Array.zip parameters names do
             match (box p) with
             | :? Parameter as p -> 
-                m.parametersDict.add(n, p)
+                m.parameters.add(n, p)
             | :? Model as mm ->
-                m.SubModelsDict.Add(n, mm)
-                m.parametersDict.add(mm.parametersDict.map(fun (nn, pp:Parameter) -> (n + "__" + nn, pp)))
+                m.subModels.Add(n, mm)
+                m.parameters.add(mm.parameters.map(fun (nn, pp:Parameter) -> (n + "__" + nn, pp)))
             | _ -> failwithf "Unsupported type. Expecting a Parameter or Model"
 
     /// <summary>TBD</summary>
-    member m.forwardDiff(derivatives:ParametersDict) = m.parametersDict.forwarddiff(derivatives)
+    member m.forwardDiff(derivatives:ParameterDict) = m.parameters.forwardDiff(derivatives)
 
     /// <summary>TBD</summary>
-    member m.reverseDiff() = m.parametersDict.reverseDiff()
+    member m.reverseDiff() = m.parameters.reverseDiff()
 
     /// <summary>TBD</summary>
-    member m.noDiff() = m.parametersDict.noDiff()
+    member m.noDiff() = m.parameters.noDiff()
 
     /// <summary>TBD</summary>
-    member m.move(?dtype, ?device, ?backend) = m.parametersDict.move(?dtype=dtype, ?device=device, ?backend=backend)
+    member m.move(?dtype, ?device, ?backend) = m.parameters.move(?dtype=dtype, ?device=device, ?backend=backend)
 
     /// <summary>TBD</summary>
-    member m.nparameters = m.parametersDict.nelement
+    member m.nparameters = m.parameters.nelement
 
     /// <summary>TBD</summary>
     abstract member forward: Tensor -> Tensor
 
     /// <summary>TBD</summary>
     member m.forwardParameters (input:Tensor) (parameters:Tensor) =
-        m.parameters <- parameters
+        m.parametersVector <- parameters
         let f = m.forward(input) in m.noDiff(); f
 
     /// <summary>TBD</summary>
@@ -262,10 +265,10 @@ type Model() =
     static member (-->) (t:Tensor, m:Model) = m.forward t
 
     /// <summary>TBD</summary>
-    member m.saveParameters(fileName) = m.parameters.save(fileName)
+    member m.saveParameters(fileName) = m.parametersVector.save(fileName)
 
     /// <summary>TBD</summary>
-    member m.loadParameters(fileName) = m.parameters <- Tensor.load(fileName)
+    member m.loadParameters(fileName) = m.parametersVector <- Tensor.load(fileName)
 
     /// <summary>TBD</summary>
     member m.save(fileName) = saveBinary m fileName
