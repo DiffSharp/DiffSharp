87fb685f573aef0687e73e4e2a02b1ae10b8726b
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2021-03-06 13:56:28 +0000

    rnn

---
+++ examples/rnn.fsx
@@ -0,0 +1,117 @@
+#!/usr/bin/env -S dotnet fsi
+
+#I "../tests/DiffSharp.Tests/bin/Debug/net5.0"
+#r "DiffSharp.Core.dll"
+#r "DiffSharp.Data.dll"
+#r "DiffSharp.Backends.Torch.dll"
+// #r "nuget: libtorch-cuda-10.2-linux-x64, 1.7.0.1"
+System.Runtime.InteropServices.NativeLibrary.Load("/home/gunes/anaconda3/lib/python3.8/site-packages/torch/lib/libtorch.so")
+
+
+open DiffSharp
+open DiffSharp.Compose
+open DiffSharp.Model
+open DiffSharp.Data
+open DiffSharp.Optim
+open DiffSharp.Util
+
+dsharp.config(backend=Backend.Torch, device=Device.CPU)
+dsharp.seed(1)
+
+let rnnShape (value:Tensor) inFeatures =
+    if value.dim <> 3 then failwithf "Expecting the input to be of shape seqLen x batchSize x inFeatures, but received input with shape %A" value.shape
+    if value.shape.[2] <> inFeatures then failwithf "Expecting input to have %A features, but received input with shape %A" inFeatures value.shape
+    let seqLen, batchSize = value.shape.[0], value.shape.[1]
+    seqLen, batchSize
+
+
+type RNNCell(inFeatures, outFeatures, ?nonlinearity, ?bias) =
+    inherit Model()
+    let nonlinearity = defaultArg nonlinearity dsharp.tanh
+    let bias = defaultArg bias true
+    let k = 1./sqrt (float outFeatures)
+    let wih = Parameter(Weight.uniform([|inFeatures; outFeatures|], k))
+    let whh = Parameter(Weight.uniform([|outFeatures; outFeatures|], k))
+    let b = Parameter(if bias then Weight.uniform([|outFeatures|], k) else dsharp.tensor([]))
+    let h = Parameter <| dsharp.tensor([])
+    do base.add([wih;whh;b],["RNNCell-weight-ih";"RNNCell-weight-hh";"RNNCell-bias"])
+
+    member _.hidden 
+        with get () = h.value
+        and set v = h.value <- v
+
+    override _.getString() = sprintf "RNNCell(%A, %A)" inFeatures outFeatures
+
+    member r.reset() = r.hidden <- dsharp.tensor([])
+
+    override r.forward(value) =
+        let seqLen, batchSize = rnnShape value inFeatures
+        if r.hidden.nelement = 0 then r.hidden <- dsharp.zeros([batchSize; outFeatures])
+        let output = Array.create seqLen (dsharp.tensor([]))
+        for i in 0..seqLen-1 do
+            let v = value.[i]
+            r.hidden <- dsharp.matmul(v, wih.value) + dsharp.matmul(h.value, whh.value)
+            if bias then r.hidden <- r.hidden + b.value
+            r.hidden <- nonlinearity r.hidden
+            output.[i] <- r.hidden
+        dsharp.stack output
+
+
+type RNN(inFeatures, outFeatures, ?nonlinearity, ?numLayers, ?bias, ?dropout, ?bidirectional) =
+    inherit Model()
+    let numLayers = defaultArg numLayers 1
+    let dropout = defaultArg dropout 0.
+    let bidirectional = defaultArg bidirectional false
+    let numDirections = if bidirectional then 2 else 1
+    let makeLayers () = Array.init numLayers (fun i -> if i = 0 then RNNCell(inFeatures, outFeatures, ?nonlinearity=nonlinearity, ?bias=bias) else RNNCell(outFeatures, outFeatures, ?nonlinearity=nonlinearity, ?bias=bias))
+    let layers = makeLayers()
+    let layersReverse = if bidirectional then makeLayers() else [||]
+    let dropoutLayer = Dropout(dropout)
+    let hs = Parameter <| dsharp.tensor([])
+    do 
+        base.add(layers |> Array.map box, Array.init numLayers (fun i -> sprintf "RNN-layer-%A" i))
+        if dropout > 0. then base.add([dropoutLayer], ["RNN-dropout"])
+
+    member _.hidden
+        with get () = hs.value
+        and set v = hs.value <- v
+
+    override _.getString() = sprintf "RNN(%A, %A, numLayers:%A)" inFeatures outFeatures numLayers
+
+    member r.reset() = r.hidden <- dsharp.tensor([])
+
+    override r.forward(value) =
+        let _, batchSize = rnnShape value inFeatures
+        if r.hidden.nelement = 0 then r.hidden <- dsharp.zeros([numLayers*numDirections; batchSize; outFeatures])
+        let newhs = Array.create (numLayers*numDirections) (dsharp.tensor([]))
+        let mutable hFwd = value
+        for i in 0..numLayers-1 do 
+            layers.[i].hidden <- r.hidden.[i]
+            hFwd <- layers.[i].forward(hFwd)
+            if dropout > 0. && i < numLayers-1 then hFwd <- dropoutLayer.forward(hFwd)
+            newhs.[i] <- layers.[i].hidden
+        let output = 
+            if bidirectional then
+                let mutable hRev = value.flip([0])
+                for i in 0..numLayers-1 do 
+                    layersReverse.[i].hidden <- r.hidden.[numLayers+i]
+                    hRev <- layersReverse.[i].forward(hRev)
+                    if dropout > 0. && i < numLayers-1 then hRev <- dropoutLayer.forward(hRev)
+                    newhs.[numLayers+i] <- layersReverse.[i].hidden
+                dsharp.cat([hFwd; hRev], 2)
+            else hFwd
+        r.hidden <- dsharp.stack(newhs)
+        output
+
+
+let rnn = RNN(32, 10, bidirectional=true)
+let x = dsharp.randn([4; 16; 32])
+print rnn.hidden.shape
+let h = x --> rnn
+print rnn.hidden.shape
+print h.shape 
+// rnn.reset()
+
+// print rnn.hidden.shape
+// let h2 = x --> rnn
+// print rnn.hidden.shape
