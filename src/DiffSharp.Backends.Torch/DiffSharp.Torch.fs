9f762ea235e6b91945b413520cf287ae1cc5f3f7
Don Syme <donsyme@fastmail.fm>
2022-02-09 19:52:11 +0000

    primitives to allow lifting from TorchSharp operations to DiffSharp operations

--- src/DiffSharp.Backends.Torch/DiffSharp.Torch.fs
+++ src/DiffSharp.Backends.Torch/DiffSharp.Torch.fs
@@ -1,5 +1,5 @@
 ﻿/// Defines extensions to the DiffSharp programming model when the Torch backend can be assumed
-namespace DiffSharp.Torch
+namespace DiffSharp
 
 open DiffSharp
 open DiffSharp.Backends.Torch
@@ -8,20 +8,26 @@ open TorchSharp
 [<AutoOpen>]
 module Extensions =
 
-    type torch.Tensor with
+    type dsharp with
 
         /// <summary>
         /// Creates a new DiffSharp tensor from the torch tensor.
         /// </summary>
-        member tt.toTensor() : Tensor =
+        static member fromTorch(tt: torch.Tensor) =
             Tensor.ofRawTensor(TorchRawTensor(tt))
 
     type Tensor with
         /// <summary>
         /// Converts the primal of a tensor to a torch tensor.
         /// </summary>
-        /// <remarks>If the tensor does not use the Torch backend an exception is raised</remarks>
-        member t.primalRawTorch() =
+        /// <remarks>
+        /// If the tensor does not use the Torch backend an exception is raised.
+        ///
+        /// Note that this operation takes the primal of the tensor. This means
+        /// code that converts to Torch tensors will not be differentiable using
+        /// DiffSharp differentiation capabilities.
+        /// </remarks>
+        member t.toTorch() =
             match t.primalRaw with
             | :? TorchRawTensor as trt -> trt.TorchTensor
             | _ -> failwith $"primalRawTorch: the input is not a DiffSharp.Backends.Torch tensor, its backend is {t.backend}"
