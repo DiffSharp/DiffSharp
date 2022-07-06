3e8e2abcaa3fdd0aaf1c9acb9c1b73b580b47b36
Deen Abiola <sir.deenicus@gmail.com>
2022-07-06 13:49:29 +0100

    fix stackoverflow crash in ModelBase.ToString()

--- src/DiffSharp.Core/Model.fs
+++ src/DiffSharp.Core/Model.fs
@@ -482,9 +482,10 @@ type ModelBase() =
         sb.Append("Model(") |> ignore
         let mutable prefix = ""
         for mm in m.descendants do
-            if mm.hasOwnParameters then
-                sb.Append(sprintf "%s%A" prefix mm) |> ignore
-                prefix <- ", "
+            if m <> mm then
+                if mm.hasOwnParameters then
+                    sb.Append(sprintf "%s%A" prefix mm) |> ignore
+                    prefix <- ", "
         sb.Append(")") |> ignore
         sb.ToString()
 
